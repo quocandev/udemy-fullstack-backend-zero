47f420e8fd51ccf72458b8dc2b5cda7dd0a70fa1
Do Quoc An <doquocan1601@gmail.com>
2025-03-08 00:57:17 +0700

    dev

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
-require('dotenv').config()
-const express = require('express')
-const configViewEngine = require('./config/viewengine')
-const webRoutes = require('./routes/web')
+require('dotenv').config();
+const express = require('express'); //commonjs
+const configViewEngine = require('./config/viewEngine');
+const webRoutes = require('./routes/web');
+const connection = require('./config/database');
 
-const app = express()
-const port = process.env.PORT || 8888 //port --> hardcode .uat .prod
-const hostname = process.env.HOSTNAME;
+const app = express();// app express
+const port = process.env.PORT || 8888; //port => hardcode . uat .prod
+const hostname = process.env.HOST_NAME;
+
 
 //config template engine
 configViewEngine(app);
 
-//Khai báo routes
+//khai báo route
 app.use('/', webRoutes);
+
+// A simple SELECT query
+connection.query(
+    'select * from Users u',
+    function (err, results, fields) {
+        console.log(">>>results= ", results); // results contains rows returned by server
+    }
+);
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
