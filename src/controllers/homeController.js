bb3bc0813b118e97cb353ad431a4c0c1d14c52ee
Do Quoc An <doquocan1601@gmail.com>
2025-03-09 11:59:40 +0700

    nodejs

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,22 @@
 
+const connection = require('../config/database');
+
 const getHomepage = (req, res) => {
     //process data
     //call model
-    res.send('Hello World with Quoc An and nodemon');
+    let users = []
+
+    connection.query(
+        'select * from Users u',
+        function (err, results, fields) {
+            users = results;
+            console.log(">>>results= ", results); // results contains rows returned by server
+
+
+            // console.log(">>>check users= ", users);
+            res.send(JSON.stringify(users));
+        }
+    );
 }
 
 const getABC = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -15,14 +15,6 @@ configViewEngine(app);
 //khai báo route
 app.use('/', webRoutes);
 
-// A simple SELECT query
-connection.query(
-    'select * from Users u',
-    function (err, results, fields) {
-        console.log(">>>results= ", results); // results contains rows returned by server
-    }
-);
-
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
