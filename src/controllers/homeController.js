90e71b331b1f23e964a212e4a0e55b87eedd40e9
Do Quoc An <doquocan1601@gmail.com>
2025-03-11 00:58:27 +0700

    nodejs

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -14,8 +14,22 @@ const getHary = (req, res) => {
 }
 
 const postCreateUser = (req, res) => {
-    console.log(">>> req.body: ", req.body);
-    res.send('create a new user')
+
+    let email = req.body.email;
+    let name = req.body.myname;
+    let city = req.body.city;
+
+    console.log(">>> email=", email, " name=", name, " city=", city);
+
+    connection.query(
+        `    INSERT INTO 
+        Users(email, name, city) 
+        VALUES(?, ?, ?); `,
+        [email, name, city],
+        function (err, results) {
+            res.send('Create user succeed!')
+        }
+    )
 }
 
 module.exports = {
